4428ebbde5e1814160e3720bf263d993f55385d4
mbobic1 <mbobic1@etf.unsa.ba>
2023-09-24 15:45:52 +0200

    pp

--- ocjenjivanje/app/register/page.js
+++ ocjenjivanje/app/register/page.js
@@ -2,17 +2,26 @@
 import React from 'react';
 import { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import { Container, Paper, Typography, Radio, RadioGroup, FormControlLabel, Button } from '@mui/material';
+import { Container, Paper, Typography, Radio, RadioGroup, FormControlLabel, Button, TextField } from '@mui/material';
 
 const Register = () => {
     const [selectedRole, setSelectedRole] = useState('student');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     const handleRoleChange = (event) => {
       setSelectedRole(event.target.value);
     };
-  
+    
+    const blackButtonStyle = {
+        backgroundColor: 'black',
+        color: 'white', // Optional: Set text color to white for contrast
+      };
+    
+
     const handleSubmit = (event) => {
-      event.preventDefault();
+        console.log("parametri "  + email + password + selectedRole);
+      //event.preventDefault();
       // Handle form submission here
     };
   return (
@@ -27,8 +36,22 @@ const Register = () => {
             <FormControlLabel value="student" control={<Radio />} label="Student" />
             <FormControlLabel value="professor" control={<Radio />} label="Professor" />
           </RadioGroup>
-          {/* Add registration fields (name, email, password, etc.) here */}
-          <Button type="submit" variant="contained" color="primary" fullWidth>
+            <TextField
+              onChange = { (e) => {setEmail(e)}}
+              label="Email"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+            />        
+            <TextField
+              onChange = { (e) => {setPassword(e)}}
+              type="password"
+              label="Password"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+            />
+          <Button type="submit" variant="contained" color="primary" style={blackButtonStyle} fullWidth>
             Register
           </Button>
         </form>
